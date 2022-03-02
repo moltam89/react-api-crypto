ecd899e96b9633e58a60bdc73252872ca6466607
tamas.molnar <tamas.molnar@liferay.com>
2022-03-02 10:43:11 +0100

    import axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
+import axios from  'axios';
+
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+
 
 function App() {
+
+
+
   return (
     <div className="App">
       <h1>API</h1>
