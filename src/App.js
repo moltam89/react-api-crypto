13689ca208324171ceceaa380d943f5dd89fff70
tamas.molnar <tamas.molnar@liferay.com>
2022-03-02 10:50:56 +0100

    Saving response to state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
 import axios from  'axios';
+import React, {useState, useEffect} from 'react';
 
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+let COINGECKO_API_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
 
 function App() {
 
+  const [coins, setCoins] = useState([])
 
+  useEffect(() => {
+    axios.get(COINGECKO_API_URL)
+      .then(res => {
+        setCoins(res.data);
+    })
+      .catch(error => console.log(error));
+  }, []);
 
   return (
     <div className="App">
       <h1>API</h1>
+
     </div>
   );
 }
