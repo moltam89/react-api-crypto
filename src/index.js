ce91d6ae007faa3bf5ba598933125ef37e3db5ba
tamas.molnar <tamas.molnar@liferay.com>
2022-03-17 11:44:32 +0100

    Copy-paste rechart

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import RechartsDemo from './RechartsDemo';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RechartsDemo />
   </React.StrictMode>,
   document.getElementById('root')
 );
