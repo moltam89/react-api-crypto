6eace52189403d777fbb65f906622f7abe55ad47
rolandpakai <roland.pakai@liferay.com>
2022-06-09 12:12:54 +0200

    Applying the coingecko design

--- src/HighChartsDemo.jsx
+++ src/HighChartsDemo.jsx
@@ -10,7 +10,7 @@ import {
   useParams
 } from "react-router-dom";
 
-const HighChartsDemo = ({coinIds, percentage = false, buttons = [1, 7, 14, 30, 180, 365]}) => {
+const HighChartsDemo = ({coinIds, percentage = false, days = [1, 7, 14, 30, 180, 365]}) => {
   let { coinId } = useParams();
 
   useEffect(() => {
@@ -24,8 +24,6 @@ const HighChartsDemo = ({coinIds, percentage = false, buttons = [1, 7, 14, 30, 1
   }, [coinIds, coinId]);
 
   const [displayedCoinIds, setDisplayedCoinIds] = useState([]);
-
-  //console.log("displayedCoinIds", displayedCoinIds ? displayedCoinIds : "");
 
   const [coinGeckoData, setCoinGeckoData] = useState([]);
   const [queryNumberOfDays, setQueryNumberOfDays] = useState(1);
@@ -142,25 +140,90 @@ const HighChartsDemo = ({coinIds, percentage = false, buttons = [1, 7, 14, 30, 1
 
   }, [displayedCoinIds, queryNumberOfDays, to]);
 
-  return (
-    <div>
-      HighChartsDemo
+  const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
 
-      <div >
-          {buttons.map(
-            button => {
-              return (
-                <Button style={{backgroundColor: "red"}} type="primary" onClick={() => setQueryNumberOfDays(button)}>
-                    {button} d
-                </Button>
-              )
-            })
-          }
+  return (
+    <div className="row no-gutters md:tw-mt-6">
+    <div className="col-lg-8 pr-md-3">
+      <h1 className="tw-flex dark:tw-text-white tw-items-center tw-mt-4 tw-mb-2 tw-text-lg md:tw-text-2xl tw-font-bold dark:tw-text-white dark:tw-text-opacity-87">
+      {capitalizeFirstLetter(coinId)} Price Chart (BTC/<span data-target="currency.currencyCode">USD</span>)
+      </h1>
+
+      <p className="tw-text-sm tw-text-gray-500 tw-mt-1 dark:tw-text-gray-400">
+        Last updated 09 June 2022, 08:01AM UTC. Currency in <span data-target="currency.currencyCode">USD</span>.
+      </p>
+    
+      <div className="filter-chart tw-flex tw-flex-wrap flex-column flex-md-row tw-justify-center justify-content-md-between" data-target="price-chart.filterChart">
+        <div className="tw-flex left-0 tw-mb-2">
+          <div className="tw-relative tw-z-0 tw-inline-flex tw-shadow-sm tw-rounded-md chart-selector" data-target="price-chart.chartMode">
+            <button type="button" className="tw-bg-white dark:tw-bg-white dark:tw-bg-opacity-5 dark:tw-text-white tw-h-8 tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-rounded-l-md tw-border-solid tw-border tw-cursor-pointer tw-border-gray-300 tw-text-sm tw-font-medium tw-text-gray-700 hover:tw-bg-gray-50 dark:hover:tw-bg-opacity-10 focus:tw-z-10 focus:tw-outline-none focus:tw-bg-gray-200 dark:tw-border-opacity-10 dark:focus:tw-bg-opacity-10 active" data-action="click->price-chart#updateChart" data-action-type="type-price">
+            Price
+            </button>
+            <button type="button" className="tw-bg-white dark:tw-bg-white dark:tw-bg-opacity-5 dark:tw-text-white tw-h-8 tw--ml-px tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-border-solid tw-border tw-cursor-pointer tw-border-gray-300 tw-text-sm tw-font-medium tw-text-gray-700 hover:tw-bg-gray-50 dark:hover:tw-bg-opacity-10 focus:tw-z-10 focus:tw-outline-none focus:tw-bg-gray-200 dark:tw-border-opacity-10 dark:focus:tw-bg-opacity-10" data-target="price-chart.marketCapButton" data-action="click->price-chart#updateChart" data-action-type="type-market-cap">
+            Market Cap
+            </button>
+            <button type="button" className="tw-bg-white dark:tw-bg-white dark:tw-bg-opacity-5 dark:tw-text-white tw-h-8 tw--ml-px tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-rounded-r-md tw-border-solid tw-border tw-cursor-pointer tw-border-gray-300 tw-text-sm tw-font-medium tw-text-gray-700 hover:tw-bg-gray-50 dark:hover:tw-bg-opacity-10 focus:tw-z-10 focus:tw-outline-none focus:tw-bg-gray-200 dark:tw-border-opacity-10 dark:focus:tw-bg-opacity-10" id="chart-toggle-trading-view" data-action="click->price-chart#updateChart" data-action-type="type-trading-view">
+            TradingView
+            </button>
+          </div>
+        </div>
+
+        <div className="tw-h-8 tw-mb-2 md:tw-mb-0 tw-relative tw-z-0 tw-inline-flex tw-shadow-sm tw-rounded-md chart-selector left-0" data-target="price-chart.toolbar">
+
+            {days.map(
+              (day, index) => {
+                let cssClass;
+                
+                if(index == 0) {
+                  cssClass = "tw-bg-white dark:tw-bg-white dark:tw-bg-opacity-5 dark:tw-text-white tw-h-8 tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-rounded-l-md tw-border-solid tw-border tw-cursor-pointer tw-border-gray-300 tw-text-sm tw-font-medium tw-text-gray-700 hover:tw-bg-gray-50 dark:hover:tw-bg-opacity-10 focus:tw-z-10 focus:tw-outline-none focus:tw-bg-gray-200 dark:tw-border-opacity-10 dark:focus:tw-bg-opacity-10 dark:hover:tw-text-white";
+                } else if(index == days.length-1) {
+                  cssClass = "tw-bg-white dark:tw-bg-white dark:tw-bg-opacity-5 dark:tw-text-white tw-h-8 tw--ml-px tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-rounded-r-md tw-border-solid tw-border tw-cursor-pointer tw-border-gray-300 tw-text-sm tw-font-medium tw-text-gray-700 hover:tw-bg-gray-50 dark:hover:tw-bg-opacity-10 focus:tw-z-10 focus:tw-outline-none focus:tw-bg-gray-200 dark:tw-border-opacity-10 dark:focus:tw-bg-opacity-10 dark:hover:tw-text-white graph-stats-btn";
+                } else {
+                  cssClass = "tw-bg-white dark:tw-bg-white dark:tw-bg-opacity-5 dark:tw-text-white tw-h-8 tw--ml-px tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-border-solid tw-border tw-cursor-pointer tw-border-gray-300 tw-text-sm tw-font-medium tw-text-gray-700 hover:tw-bg-gray-50 dark:hover:tw-bg-opacity-10 focus:tw-z-10 focus:tw-outline-none focus:tw-bg-gray-200 dark:tw-border-opacity-10 dark:focus:tw-bg-opacity-10 dark:hover:tw-text-white";
+                }
+
+                let title = day + "d";
+                if (day == 1) {
+                  title = "24h"
+                } else if (day == 365) {
+                  title = "1y"
+                }
+
+                return (
+                  <a 
+                    key={title}
+                    className={cssClass}
+                    href="#"
+                    onClick={() => setQueryNumberOfDays(day)}
+                    >
+                      {title}
+                    </a>
+                )
+              })
+            }
+        </div>
+      </div>
+      <div className="filter-chart tw-flex tw-justify-between md:tw-justify-end">
+        <div className="tailwind-reset tw-relative tw-z-0 tw-inline-flex tw-shadow-sm tw-rounded-md chart-selector tw-mb-2" data-target="price-chart.chartModeButtonGroup">
+          <button type="button" className="tw-bg-white dark:tw-bg-white hover:tw-bg-gray-50 dark:tw-bg-opacity-5 dark:tw-text-white dark:hover:tw-bg-opacity-10 tw-h-8 tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-rounded-l-md tw-border-solid tw-border tw-cursor-pointer tw-border-gray-300 tw-text-sm tw-font-medium tw-text-gray-700 hover:tw-bg-gray-50 dark:hover:tw-bg-opacity-10 focus:tw-z-10 focus:tw-outline-none focus:tw-bg-gray-200 dark:tw-border-opacity-10 dark:focus:tw-bg-opacity-10 active">
+            <i className="fas fa-chart-line"></i>
+          </button>
+          <button type="button" className="btn btn-icon shadow-none tw-bg-white dark:tw-bg-white hover:tw-bg-gray-50 dark:tw-bg-opacity-5 dark:hover:tw-bg-opacity-10 tw-rounded-md tw-rounded-l-none md:tw-rounded-none tw-border-solid tw-border tw-cursor-pointer tw-border-gray-300 dark:tw-border-opacity-10 center tw-w-11 dark:focus:tw-bg-opacity-10 focus:tw-z-10 focus:tw-outline-none focus:tw-bg-gray-200 dark:focus:tw-bg-opacity-10">
+            <i className="fas fa-chart-line"></i>
+          </button>
+          <a className="tw-hidden d-md-inline-block tw-bg-white hover:tw-bg-gray-50 dark:tw-bg-white dark:tw-bg-opacity-5 dark:tw-text-white tw-h-8 tw--ml-px tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-rounded-r-md tw-border-solid tw-border tw-cursor-pointer tw-border-gray-300 tw-text-sm tw-font-medium tw-text-gray-700 hover:tw-bg-gray-50 dark:hover:tw-bg-opacity-10 focus:tw-z-10 focus:tw-outline-none focus:tw-bg-gray-200 hover:tw-bg-gray-50 dark:tw-border-opacity-10 dark:focus:tw-bg-opacity-10 dark:hover:tw-text-white coingecko tw-px-2 tw-pt-1.5" 
+              data-target="currency.currencyLink" href="https://www.coingecko.com/en/coins/bitcoin/usd#panel">
+            <i className="far fa-expand-arrows"></i>
+          </a>
+          </div>
       </div>
 
       <HighchartsReact highcharts={Highcharts}  options={options} />
       <InputNumber placeholder="from"defaultValue={1} onChange={queryNumberOfDaysOnChange} />
       <InputNumber placeholder="to"  onChange={toOnchange} />
+    </div>
     </div>
   );
 }
